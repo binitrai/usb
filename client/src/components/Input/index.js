e7ecf2836bda24ec814553943aa5922733001979
Saquib Ajaz <saquibbca@gmail.com>
2020-01-16 16:49:15 +0530

    login box

---
+++ client/src/components/Input/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './index.scss';
+
+export function InputGroup({ children, direction }) {
+    return (
+        <div className={"input-group "+direction}>
+            {children}
+        </div>
+    )
+}
+
+export function Input({ type, placeholder }) {
+    return (
+        <div className="input-item">
+            <input type={type} placeholder={placeholder} />
+        </div>
+    )
+}
+
+export function ButtonGroup({ children, align }) {
+    return (
+        <div className={"btn-container "+align}>
+            {children}
+        </div>
+    )
+}
+
+export function Button({ theme = "", type, children, onClick, push = "" }) {
+    return (
+        <div className={"btn-item "+push}>
+            <button className={theme} type={type} onClick={onClick}>{children}</button>
+        </div>
+    )
+}
+
+export function InputLink({ href, children, onClick, push = "" }) {
+    return (
+        <div className={"btn-item "+push}>
+            <a href={href} onClick={onClick}>{children}</a>
+        </div>
+    )
+} 
--- client/src/components/Login/LoginComponent.js
+++ client/src/components/Login/LoginComponent.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Page from '../Page';
+import {InputGroup, Input, Button, InputLink, ButtonGroup} from '../Input';
+
 import './index.scss';
 
 function LoginComponent({ loginName }) {
   return (
     <Page header={false}>
       <div className="login-container">
-Login Page
-        {loginName}
+        <div className="login-box">
+          <div className="login-text">Welcome Back</div>
+          <div className="login-email-container">
+              <form>
+                <InputGroup direction="vertical">
+                  <Input type="text" placeholder="Email" />
+                  <Input type="password" placeholder="Password" />
+                  <ButtonGroup align="auto">
+                    <Button type="submit" onClick={()=>alert("Logged In")}>Log In</Button>
+                    <InputLink href="#">Forget Your Password?</InputLink>
+                  </ButtonGroup>
+                </InputGroup>
+              </form>
+          </div>
+        </div>
+        <div className="or-line"><span>or</span></div>
+        <Button theme="white" type="submit" onClick={()=>alert("Logged In")}>Don't have an account? Sign up</Button>
       </div>
     </Page>
   );
